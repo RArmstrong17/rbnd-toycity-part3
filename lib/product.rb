bd7c80ce8892afc50b25777e32e2ec3f504a978f
Rich Armstrong <rtarmstrong11@gmail.com>
2016-04-03 14:08:25 -0400

    Products
    Methods to initialize, search, and return specific properties of each
    product.

--- lib/product.rb
+++ lib/product.rb
@@ -1,38 +1,60 @@
 class Product
-  @@products = Array.new{Hash.new}
   attr_accessor :title, :price, :stock
-    def initialize(title: title, price: price, stock: stock)
-      @product_hash = Hash.new{}
-      @product_hash[:title] = title
-      @product_hash[:price] = price
-      @product_hash[:stock] = stock
-      add_to_products
+  @@products = Array.new
+    def initialize(options = {})
+      @title = options[:title]
+      @price = options[:price]
+      @stock = options[:stock]
+      add_to_products(options)
     end
 
     def self.all
-      return @@products
+      return @@products # Return the Array of initialized products.
     end
 
     def self.find_by_title(title)
-      @@products.each_with_index do |product, index|
-        if product[:title] == title
-          return @@products[index]
-        end
-      end
+      @@products.find{|product| product[:title] == title} # Finds the product via title.
+    end
+
+    def self.in_stock
+      @@products.select!{|product| product[:stock] > 0} # Selects the
     end
 
   private
-    def add_to_products
+    def add_to_products(options = {}) # Pass intital arguments through a private method.
       titles = []
-      if @@products.empty?
-        @@products.push(@product_hash)
+      if @@products.empty? # Always push first product entered.
+        @@products.push(options)
       else
         titles = @@products.map{|title| title[:title]}
-          if titles.include?(@product_hash[:title])
-            raise DuplicateProductError, "#{@product_hash[:title]} already exists."
+          if titles.include?(options[:title]) # Check to see if product was already entered by matching its title.
+            raise DuplicateProductError, "#{options[:title]} already exists."
           else
-            @@products.push(@product_hash)
+            @@products.push(options)
           end
       end
     end
+end
+
+class Object # Methods for products objects. 
+  def title
+      return self[:title]
+  end
+
+  def price
+    return self[:price]
+  end
+
+  def stock
+    return self[:stock]
+  end
+
+  def in_stock?
+    if self[:stock] == 0
+      return false
+    else
+      true
+    end
+  end
+
 end
